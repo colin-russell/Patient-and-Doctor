ce6dd8dfeb3dd18c8be2c8959a2a6bd9e9a8ecf8
Colin Russell <colinrussell101@gmail.com>
2018-04-12 13:02:42 -0400

    Initial Commit

---
+++ Patient and Doctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Patient and Doctor
+//
+//  Created by Colin on 2018-04-12.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
